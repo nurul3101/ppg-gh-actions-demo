e91ff3d4c9b241221ad43c3f6c380ee3e2e08adf
Nurul Sundarani <sundarani@prisma.io>
2025-07-30 20:13:26 +0530

    Update queries.ts

--- src/queries.ts
+++ src/queries.ts
@@ -12,7 +12,7 @@ async function main() {
   const user1 = await prisma.user.create({
     data: {
       email: user1Email,
-      name: "Alice",
+      name: "Alice1",
       posts: {
         create: {
           title: "Join the Prisma community on Discord",
